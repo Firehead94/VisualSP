5d5c7727332fe39bac9f74357188cb44c166c440
Justin Scott <justinjonscott@gmail.com>
2019-03-22 16:49:10 -0400

    Update WindowBar.py

--- src/gui/WindowBar.py
+++ src/gui/WindowBar.py
@@ -139,9 +139,7 @@ if __name__ == '__main__':
     box.move(60,60)
     l=QtWidgets.QVBoxLayout(box.contentWidget())
     l.setContentsMargins(0, 0, 0, 0)
-    edit=QtWidgets.QLabel("""I would've did anything for you to show you how much I adored you
-But it's over now, it's too late to save our loveJust promise me you'll think of me
-Every time you look up in the sky and see a star 'cuz I'm  your star.""")
+    edit=QtWidgets.QLabel("edit")
     l.addWidget(edit)
     box.show()
     app.exec_()
