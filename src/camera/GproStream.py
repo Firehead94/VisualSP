da7c13000a1b500f7d25af0a68c22be414cb2f34
Justin Scott <justinjonscott@gmail.com>
2019-04-07 14:52:28 -0400

    Update GproStream.py

--- src/camera/GproStream.py
+++ src/camera/GproStream.py
@@ -29,9 +29,9 @@ class GproStream(QtWidgets.QWidget):
         tracker = DetectTracker.DetectAndTrack(cap)
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
-        fps = cap.get(5)
+        fps = cap.get(30)
         vid = cv.VideoWriter_fourcc(*'MJPG') # writng the video file
-        out = cv.VideoWriter(self.folderPath + self.File_Output, vid, fps, (frame_width, frame_height))
+        out = cv.VideoWriter(fileLoc, vid, fps, (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))))
         # File = open(complete_save, "w")
 
         if cap.isOpened() != True:
@@ -41,7 +41,7 @@ class GproStream(QtWidgets.QWidget):
             ret, frame = cap.read()
             tracked = tracker.trackStuff(ret,frame)
             ##gray = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
-            cv.imshow("GoPro OpenCV", tracked)
+            cv.imshow("GoPro OpenCV", frame)
             out.write(tracked)
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
